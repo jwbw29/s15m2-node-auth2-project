bd4a987975e50a9db67312e51fd605bd53c985e3
Justin Byrd <justinbyrd7@gmail.com>
2023-12-14 16:41:32 -0600

    ip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -4,20 +4,21 @@ const { JWT_SECRET } = require("../secrets"); // use this secret!
 
 // [ ] 4. restricted
 const restricted = (req, res, next) => {
-  const token = req.headers.authorization;
-  console.log(token);
-  if (token) {
-    JWT_SECRET.verify(token, JWT_SECRET, (err, decoded) => {
-      if (err) {
-        next({status: 401, message: "Token invalid"})
-      } else {
-        req.decodedJwt = decoded;
-        next();
-      }
-    })
-  } else {
-    next({status: 401, message: "Token required"})
-  }
+  // const token = req.headers.authorization;
+  // console.log(token);
+  // if (token) {
+  //   JWT_SECRET.verify(token, JWT_SECRET, (err, decoded) => {
+  //     if (err) {
+  //       next({status: 401, message: "Token invalid"})
+  //     } else {
+  //       req.decodedJwt = decoded;
+  //       next();
+  //     }
+  //   })
+  // } else {
+  //   next({status: 401, message: "Token required"})
+  // }
+  next()
   /*
     If the user does not provide a token in the Authorization header:
     status 401
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,34 +4,6 @@ const db = require("../../data/db-config.js");
 
 // [x] 1. find()
 function find() {
-  /**
-
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
-
-    /*
-    select 
-      user_id,
-      username,
-      role_name
-    from users as u
-    left join roles as r
-      on u.role_id = r.role_id
-    */
    return db('users as u')
     .select('user_id', 'username', 'role_name')
     .leftJoin('roles as r', 'u.role_id', 'r.role_id')
@@ -54,18 +26,12 @@ function findBy(filter) {
    */
 }
 
-// [ ] 3. findById(user_id)
+// [x] 3. findById(user_id)
 function findById(user_id) {
-  /**
-    You will need to join two tables.
-    Resolves to the user with the given user_id.
-
-    {
-      "user_id": 2,
-      "username": "sue",
-      "role_name": "instructor"
-    }
-   */
+  return db('users as u')
+  .select('user_id', 'username', 'role_name')
+  .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+  .where('user_id', user_id).first()
 }
 
 /**
